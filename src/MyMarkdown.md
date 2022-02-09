abbbaa2d8eae9b5acd1a992524f62dee5936f3c5
julia wu <juliawu@rakuya.com.tw>
2022-02-09 11:24:42 +0800

    vite-plugin-vue-md

---
+++ src/MyMarkdown.md
@@ -0,0 +1,7 @@
+# Hello World
+
+This is some markdown
+
+* list 1
+* list 2
+* list 3
---
+++ vite-plugin-vue-md.js
@@ -0,0 +1,13 @@
+import { marked } from "marked";
+export default (userOptions = { wrapperElement: "div" }) => {
+  const { wrapperElement: el } = userOptions;
+  return {
+    name: "vite-plugin-vue-md",
+    enforce: "pre",
+    transform(raw, id) {
+      if (/\.(md)$/.test(id)) {
+        return `<template><${el}>${marked(raw)}</${el}></template>`;
+      }
+    },
+  };
+};
